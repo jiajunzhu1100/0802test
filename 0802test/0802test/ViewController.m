a95b35e342e3be38c693cd65e63e836e85f4c555
zhujiajun <jiajun.zhu@nexttao.com>
2015-08-02 15:49:04 +0800

    as
    as

--- 0802test/0802test/ViewController.m
+++ 0802test/0802test/ViewController.m
@@ -41,13 +41,13 @@
     [self.sv addSubview:fb.view];
     
     sb=[[sbViewController alloc]init];
-    sb.view.frame=CGRectMake(0, 500, 600, 200);
+    sb.view.frame=CGRectMake(0, 550, 600, 200);
 
     [self.sv addSubview:sb.view];
     
     
     tb=[[tbViewController alloc]init];
-    tb.view.frame=CGRectMake(0, 700, 560, 100);
+    tb.view.frame=CGRectMake(0, 750, 560, 100);
 
     [self.sv addSubview:tb.view];
     
@@ -58,27 +58,27 @@
     ll=[[UILabel alloc]init];
     ll.frame=CGRectMake(18, 60, 5, 13);
     ll.backgroundColor=[UIColor redColor];
-    [self.view addSubview:ll];
+    [self.sv addSubview:ll];
     bb=[[UILabel alloc]init];
     bb.frame=CGRectMake(38, 60, 100, 20);
     bb.text=@"正在售票";
-    [self.view addSubview:bb];
+    [self.sv addSubview:bb];
     ss=[[UILabel alloc]init];
     ss.frame=CGRectMake(18, 540, 5, 13);
     ss.backgroundColor=[UIColor redColor];
-    [self.view addSubview:ss];
+    [self.sv addSubview:ss];
     sss=[[UILabel alloc]init];
     sss.frame=CGRectMake(38, 540, 100, 20);
     sss.text=@"即将上映";
-    [self.view addSubview:sss];
+    [self.sv addSubview:sss];
     tt=[[UILabel alloc]init];
     tt.frame=CGRectMake(18, 1060, 5, 13);
     tt.backgroundColor=[UIColor redColor];
-    [self.view addSubview:tt];
+    [self.sv addSubview:tt];
     ttt=[[UILabel alloc]init];
     ttt.frame=CGRectMake(38, 1060, 100, 20);
     ttt.text=@"精彩活动";
-    [self.view addSubview:ttt];
+    [self.sv addSubview:ttt];
     
 
 }
